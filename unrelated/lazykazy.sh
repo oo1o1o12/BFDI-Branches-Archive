2b34a4aa29d38fd317265e637c18555086e20157
oo1o1o12 <103213250+oo1o1o12@users.noreply.github.com>
2026-09-02 15:26:01 +0300

    repurpose this repo

--- unrelated/lazykazy.sh
+++ unrelated/lazykazy.sh
@@ -1,14 +1,13 @@
 set -e
 cd "$(dirname "$0")"
 cd "../main things"
-mkdir build
-cp "BFDI Branches.zip" build
-cd build
-unzip "BFDI Branches.zip"
-cd "BFDI Branches.app/Contents/Resources"
+#mkdir build
+#cp "BFDI Branches.zip" build
+#cd build
+#unzip "BFDI Branches.zip"
+#cd "BFDI Branches.app/Contents/Resources"
 curl -o "BFDI Branches.pck" https://bfdibranches.com/new/bfdibranches.pck
-cd ../../..
-echo 'From this folder, run `xattr -cr BFDI\ Branches.app` in Terminal to allow MacOS to open it.' > README.txt
-zip -r -q branches.zip "BFDI Branches.app" README.txt
-mv branches.zip ../..
+#echo 'From this folder, run `xattr -cr BFDI\ Branches.app` in Terminal to allow MacOS to open it.' > README.txt
+zip -r -q branches.zip "BFDI Branches.pck"
+mv branches.zip ..
 echo ok
