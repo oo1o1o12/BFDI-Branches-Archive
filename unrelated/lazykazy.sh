5a802a059b595cdcfc4d2bb42a4dbdbc2385ecdc
oo1o1o12 <103213250+oo1o1o12@users.noreply.github.com>
2026-09-02 15:21:31 +0300

    darn

--- unrelated/lazykazy.sh
+++ unrelated/lazykazy.sh
@@ -6,7 +6,7 @@ cp "BFDI Branches.zip" build
 cd build
 unzip "BFDI Branches.zip"
 cd "BFDI Branches.app/Contents/Resources"
-curl -o "BFDI Branches.pck" https://bfdibranches.com/bfdibranches.pck
+curl -o "BFDI Branches.pck" https://bfdibranches.com/new/bfdibranches.pck
 cd ../../..
 echo 'From this folder, run `xattr -cr BFDI\ Branches.app` in Terminal to allow MacOS to open it.' > README.txt
 zip -r -q branches.zip "BFDI Branches.app" README.txt
